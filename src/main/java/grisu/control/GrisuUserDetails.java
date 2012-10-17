d7b6bacc1e9d2011ecdad460616a5f20d94e9fdc
Markus Binsteiner <m.binsteiner@auckland.ac.nz>
2012-10-18 08:57:39 +1300

    Shuffling method calls around, otherwise credential properties get written to file.

--- src/main/java/grisu/control/GrisuUserDetails.java
+++ src/main/java/grisu/control/GrisuUserDetails.java
@@ -58,8 +58,9 @@ public class GrisuUserDetails implements UserDetails {
 		// final MyProxy myproxy = new MyProxy(myProxyServer, port);
 		try {
 			myLogger.debug("Getting delegated proxy from MyProxy...");
-			AbstractCred cred = new MyProxyCred(username, password.toCharArray(), myProxyServer, port, lifetime);
+			AbstractCred cred = new MyProxyCred(username, password.toCharArray(), myProxyServer, port);
 			cred.setSaveDetails(false);
+			cred.setProxyLifetimeInSeconds(lifetime);
 			cred.init();
 			// proxy = myproxy.get(username, password, lifetime);
 			final int remaining = cred.getRemainingLifetime();
